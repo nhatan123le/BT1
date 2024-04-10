fc58d86f916ebdf133cd3e7af470600f5f30a92c
nhatan123le <150102654+nhatan123le@users.noreply.github.com>
2024-04-11 01:31:41 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Project_1 from './src/Project_1';
+import Project_2 from './src/Project_2';
+import Project_3 from './src/Project_3';
+import Project_4 from './src/Project_4';
+import Project_5 from './src/Project_5';
+import Project_6 from './src/Project_6';
+import Project_7 from './src/Project_7';
+import Project_9 from './src/Project_9';
+const App =() =>{  
+  return (
+  // <Project_1/>
+  // <Project_2/>
+  //  <Project_3/>
+  // <Project_4/>
+  // <Project_5/>
+  //  <Project_6/> 
+  <Project_7/>
+  // <Project_9/>
+
+  );
+}
+export default App;
+
